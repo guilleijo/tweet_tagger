959ea689e920e246b2b0e7e96e2d6dc5d8389003
Guillermo Eijo <guillermoeijo@gmail.com>
2021-08-11 17:07:08 -0300

    Remove commented code

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,19 +1,7 @@
 from django.urls import path
-
 from .views import HomeView
 
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home_page"),
 ]
-
-
-# from django.urls import path
-
-# from .views import index_view, tweet_view
-
-
-# urlpatterns = [
-#     path("", index_view, name="home_page"),
-#     path("tweet/", tweet_view, name="tweet"),
-# ]
